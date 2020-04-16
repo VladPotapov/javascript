05c65dc55ac9ff6af113ef55267bb7cc155b7f26
Roman <kotelnikovrm@gmail.com>
2020-04-16 10:59:18 +0800

    update script in js_for_children

--- js_for_children/script.js
+++ js_for_children/script.js
@@ -16,4 +16,31 @@ var dannie = function () {
     console.log(true && false);
 }
 
-dannie();
+//dannie();
+
+let brothers = 1, sisters = 3, candy = 8;
+
+function division(x, y, z) {
+    return x / (y + z);
+}
+
+let res = division(candy, brothers, sisters)
+console.log(res);
+
+function multiplication(x, y, z) {
+    return (x + y) * z;
+}
+
+let shariki = multiplication(15, 9, 2);
+console.log(shariki);
+
+let name;
+console.log(name);
+
+let secondsInMinutes = 60;
+let minutesInHour = 60;
+let secondsInAnHour = secondsInMinutes * minutesInHour;
+let hoursInDay = 24;
+let secondsInDay = secondsInAnHour * hoursInDay;
+let daysInYear
+console.log(secondsInDay);
