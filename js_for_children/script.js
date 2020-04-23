fff10bbfa534ede8e3840d2e19ae29578ced2f6c
Roman <kotelnikovrm@gmail.com>
2020-04-23 14:59:06 +0800

    разобрался в инкрименте и дикрименте

--- js_for_children/script.js
+++ js_for_children/script.js
@@ -42,5 +42,35 @@ let minutesInHour = 60;
 let secondsInAnHour = secondsInMinutes * minutesInHour;
 let hoursInDay = 24;
 let secondsInDay = secondsInAnHour * hoursInDay;
-let daysInYear
-console.log(secondsInDay);
+let daysInYear = 365;
+let secondsInYear = secondsInDay * daysInYear;
+let age = 36 * secondsInYear;   //1135296000 возвраст в милисекундах
+
+//инкримент, дикримент
+function ink_dik() {
+    num_in1 = 0;
+    num_in2 = 0;
+    console.log("Инкримент");
+    console.log(0 + ++num_in1); //1
+    console.log(0 + num_in2++); //0
+    console.log(num_in1 + " : " + num_in2); //1 : 1
+
+    /** 
+     * сначало выведит 2 : 1
+     * после прибавит к num_in2 + 1
+     */
+    console.log(++num_in1 + " : " + num_in2++);
+    console.log((++num_in1) + 1);   //4
+    console.log((num_in2++) + 1);   //3
+
+    console.log("\nДикримент");
+    num_dik1 = 10;
+    num_dik2 = 10;
+    console.log(--num_dik1 - 1);    //8
+    console.log(num_dik2-- - 1);    //9
+    console.log(num_dik1 + " : " + num_dik2);   //9 : 9
+    console.log(--num_dik1 + " : " + num_dik2--);
+
+}
+
+ink_dik();
--- new_javascript/1/5/2/script.js
+++ new_javascript/1/5/2/script.js
@@ -1,2 +1,8 @@
-num = 2 + 2 ? 4 : 5;
-document.write(num);
+num = 2 + 2 ? 4 : 5;    //4
+num1 = "3" * "5";   //15
+num2 = "3" * 2; //6
+num3 = "3" * false; //0
+num4 = "1" + 0; //"10"
+num5 = "1" + true   //"1true"
+str = "abcde" - "e";    //NaN
+document.write(str);
